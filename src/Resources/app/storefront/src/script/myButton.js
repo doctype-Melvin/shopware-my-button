8ff0fd276d2d3d204364b393c811b0c6d6160239
doctype-Melvin <doctype.melvin@gmail.com>
2023-08-15 17:28:22 +0200

    add button appearance toggle after add to cart click

--- src/Resources/app/storefront/src/script/myButton.js
+++ src/Resources/app/storefront/src/script/myButton.js
@@ -1,12 +1,35 @@
-import AddToCart from "src/plugin/add-to-cart/add-to-cart.plugin";
+import AddToCartPlugin from "src/plugin/add-to-cart/add-to-cart.plugin";
+import HttpClient from "src/service/http-client.service";
+import DomAccess from "src/helper/dom-access.helper";
 
-export default class MyButton extends AddToCart {
+export default class MyButton extends AddToCartPlugin {
 	static options = {
 		buttonInitialTextContent: `In den Warenkorb`,
 		buttonInitialColor: `#0b539b`,
 		buttonProcessTextContent: `Wird in den Warenkorb gelegt`,
 		buttonProcessColor: `#595959`,
 	};
+
+	init() {
+		this._client = new HttpClient(window.accessKey, window.contextToken);
+		this.PluginManager = window.PluginManager;
+		this._cartEl = DomAccess.querySelector(document, ".header-cart");
+
+		const allBuyButtons = [...document.querySelectorAll(".btn-buy")];
+		allBuyButtons.forEach((button) => {
+			console.info("It Works!");
+			button.textContent = this.options.buttonInitialTextContent;
+			button.addEventListener("click", () => {
+				this.updateButtonAppearance(button);
+				setTimeout(() => {
+					this.restoreButtonAppearance(button);
+					this._openOffCanvasCart;
+				}, 1000);
+			});
+		});
+
+		super.init();
+	}
 
 	updateButtonAppearance(button) {
 		button.textContent = this.options.buttonProcessTextContent;
@@ -18,16 +41,27 @@ export default class MyButton extends AddToCart {
 		button.style.backgroundColor = this.options.buttonInitialColor;
 	}
 
-	init() {
-		super.init();
-		console.info(window.PluginManager.getPluginInstancesFromElement(this.el));
-		const allBuyButtons = [...document.querySelectorAll(".btn-buy")];
-		allBuyButtons.forEach((button) => {
-			button.textContent = this.options.buttonInitialTextContent;
-			button.addEventListener("click", () => {
-				this.updateButtonAppearance(button);
-				setTimeout(() => this.restoreButtonAppearance(button), 1000);
-			});
-		});
+	_openOffCanvasCart(instance, requestUrl, formData) {
+		this._client.post(
+			requestUrl,
+			formData,
+			this._afterAddItemToCart.bind(this)
+		);
+	}
+
+	_afterAddItemToCart() {
+		this._refreshCartValue();
+	}
+
+	_refreshCartValue() {
+		const cartWidgetEl = DomAccess.querySelector(
+			this._cartEl,
+			"[data-cart-widget]"
+		);
+		const cartWidgetInstance = this.PluginManager.getPluginInstanceFromElement(
+			cartWidgetEl,
+			"CartWidget"
+		);
+		cartWidgetInstance.fetch();
 	}
 }
